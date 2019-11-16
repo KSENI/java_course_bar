c3e4d1253253c9408acfccb34fdd036bb40e96c3
Kseniia <kse1non@gmail.com>
2019-11-16 19:07:26 +0500

    расстояние между точками

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static double distance(Point p1, Point p2) {
+        double deltaX = Math.abs(p1.x - p2.x);
+        double deltaY = Math.abs(p1.y - p2.y);
+        if (deltaX > 0 && deltaY > 0) {
+            return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        } else if (deltaX == 0) {
+            return deltaY;
+        } else {
+            return deltaX;
+        }
+    }
+}
--- sandbox/src/main/java/World.java
+++ sandbox/src/main/java/World.java
@@ -1,5 +1,14 @@
 public class World {
 	public static void main(String[] args) {
-		System.out.println("Зеброконь тыг-дык тыг-дык");
-	}
+        printPointAndDistance(new Point(3, 5), new Point(7, 8));
+        printPointAndDistance(new Point(1, 5), new Point(1, 8));
+        printPointAndDistance(new Point(3, 1), new Point(7, 1));
+        printPointAndDistance(new Point(0, 0), new Point(0, 0));
+    }
+
+    private static void printPointAndDistance(Point point1, Point point2) {
+        double distance = Point.distance(point1, point2);
+        System.out.println("Первая точка: x=" + point1.x + ", y=" + point1.y +
+                " Вторая точка: x=" + point2.x + ", y=" + point2.y + " Расстояние между точками = " + distance);
+    }
 }
