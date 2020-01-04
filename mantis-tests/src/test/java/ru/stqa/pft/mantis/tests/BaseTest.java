227f3c74d872c369f4fe4f55fdd989da75182a63
Kseniia <kse1non@gmail.com>
2020-01-04 13:38:20 +0500

    добален шаблон для тестов на mantis

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/BaseTest.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.mantis.tests;
+
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.IOException;
+
+public class BaseTest {
+
+    protected final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws IOException {
+        app.init();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        app.stop();
+    }
+}
